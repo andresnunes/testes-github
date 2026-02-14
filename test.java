a8f07fcc21852c92a2e5e7b5de27a352f50d672c
André Santana Nunes <38633722+andresnunes@users.noreply.github.com>
2026-02-13 21:32:46 -0300

    teste

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
-//
-//class Main {
-//    public static void main(String[] args) {
-//        System.out.printf("Hello World");
-//    }
-//}
+
+class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+    }
+}
